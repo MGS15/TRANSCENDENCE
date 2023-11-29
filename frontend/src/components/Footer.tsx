458e8d3904993476a6858797592d949b6285b97b
Aymane-1 <aechafii@student.1337.ma>
2023-11-29 04:44:48 +0100

    -fix: Styling tweaks

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 
 export default function Footer(){
 	return (
-		<footer className="max-w-[1536px] min-[0px]:my-5 min-[0px]:mx-5 lg:m-auto flex flex-col place-content-center font-pixelify h-[90px] lg:my-6 border-2 border-black rounded-xl border-solid">
-			<h1 className="text-2xl font-bold text-center text-primary">TRANSCENDENCE</h1>
+		<footer className="max-w-[1536px] min-[0px]:my-5 min-[0px]:mx-5 lg:m-auto flex flex-col place-content-center font-pixelify h-[90px] lg:my-6 border-solid border-2 border-black shadow-[2px_4px_0px_0px_#000301]">
+			<h1 className="animate-pulse text-2xl font-bold text-center text-primary">TRANSCENDENCE</h1>
 		</footer>
 	);
 }
--- frontend/src/components/HomePage/FeaturesBlocComponent.tsx
+++ frontend/src/components/HomePage/FeaturesBlocComponent.tsx
@@ -1,8 +1,8 @@
 
 export default function FeaturesBlocks(props:any){
 	return (
-		<div className={`Ft transition duration-500 ease ${props.Color} font-pixelify flex flex-col p-12 border-solid border-black border-2 text-2xl mb-10 gap-y-5 w-full`}>
-			<img src={props.Pic} className='OV bg-white' alt='pic'></img>
+		<div className={`Ft transition duration-500 ease ${props.Color} font-pixelify flex flex-col p-12 text-2xl mb-10 gap-y-5 w-full border-solid border-2 border-black shadow-[2px_4px_0px_0px_#000301]`}>
+			<img src={props.Pic} className='OV bg-white border-solid border-2 border-black ' alt='pic'></img>
 			<h1 className='text-3xl font-medium'>{props.FeatureTitle}</h1>
 			<p> {props.text} </p>
 		</div>
