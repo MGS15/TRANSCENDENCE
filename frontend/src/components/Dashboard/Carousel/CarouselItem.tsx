d53d3ef2afd5cf818e4bf03a5c12da884241210f
Aymane-1 <aechafii@student.1337.ma>
2023-12-17 23:23:32 +0100

    -fix: More tweaks

--- frontend/src/components/Dashboard/Carousel/CarouselItem.tsx
+++ frontend/src/components/Dashboard/Carousel/CarouselItem.tsx
@@ -59,7 +59,7 @@ export default function Tooltip({children, description} : {children:any, descrip
 export const CarouselItem = ({ item } : { item: any }) => {
   return (
     <div className="snap-center m-auto carousel-item inline-flex flex-col place-items-center justify-around h-[345px]">
-      <img className="carousel-img min-[0px]:w-[160px] min-[0px]:h-[190px] md:w-[280px] md:h-[350px] p-6 md:p-12 drop-shadow-[25px_30px_3px_rgba(0,0,0,0.40)]" src={item.icon}/>
+      <img className="carousel-img min-[0px]:w-[160px] min-[0px]:h-[190px] md:w-[280px] md:h-[340px] p-6 md:p-12 drop-shadow-[25px_30px_3px_rgba(0,0,0,0.40)]" src={item.icon}/>
         <h1 className="mb-12 w-full carousel-item selection:bg-buttonColor min-[0px]:text-xl xl:text-2xl font-Nova font-bold hover:duration-700 hover:ease-in-out delay-[510ms]">
           <Tooltip description={item.description}>
             {item.title}
--- frontend/src/components/Dashboard/Ladder/LadderComponent.tsx
+++ frontend/src/components/Dashboard/Ladder/LadderComponent.tsx
@@ -3,7 +3,7 @@ import users from './Users'
 
 export default function Global({contestant, LadderTitle} : {contestant : any, LadderTitle: any}){
 	return (
-		<div className="max-w-[1536px] m-8 p-10 border-solid border-4 h-[60rem] Ft bg-[#FEFDFA]">
+		<div className="max-w-[1536px] m-8 p-10 border-solid border-4 h-[60rem] Ft bg-[#F6F4F0]">
 			<h1 className='min-[0px]:text-lg xl:text-2xl text-3xl text-center font-Nova font-black mb-1 border-solid border-4 p-6 bg-white'>{LadderTitle}</h1>
 				<div className=" overflow-y-auto h-[48rem] scrollbar-none FF cursor-row-resize">
 					{users.map((user, index) => (
