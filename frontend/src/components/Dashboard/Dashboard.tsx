d2a729fb8430f91bf1e2e9b692a44a5a0a239fb6
Aymane-1 <aechafii@student.1337.ma>
2024-01-08 02:20:55 +0100

    -fix: fetch Stats data filtered by monthly timestamp | plus some tweaks

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,29 +1,28 @@
-import Navbar from "../Navbar";
 import Footer from "../Footer";
 import ProfileDiv from "./UserProfile/UserProfile";
 import Carousel from "./PongAchievements/Carousel";
 import Stats from "./PongStats/Stats";
 import Ladder from "./Ladder/LeaderBoard";
-import contestants from "./Ladder/LeaderBoard";
 import History from "./matchHistory/matchHistory";
 import { useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { currentUser } from "../Context/AuthContext";
-import { use } from "matter-js";
 import { toast } from "react-toastify";
 import { ip } from "../../network/ipaddr";
 import IUser from "../../types/User";
+import { Histo } from "../../types/yearlyres";
+
 
 const useGetGamingData = async (setgdata: any, nickname: string | undefined) => {
 	useEffect(() => {
-		fetch(`http://${ip}3001/profile/${nickname}/Stats`, {
+		fetch(`http://${ip}3001/profile/${nickname}/GamingHistory`, {
 			method: "GET",
 			credentials: "include",
 		})
 			.then((data) => data.json())
 			.then((data) => {
 				setgdata(data);
-				console.log(data);
+
 			});
 		}, []);
 };
@@ -37,6 +36,7 @@ const useGetFLadderData = async (setfladder: any, nickname: string | undefined) 
 			.then((data) => data.json())
 			.then((data) => {
 				setfladder(data);
+
 			});
 		}, []);
 };
@@ -50,6 +50,7 @@ const useGetLadderData = async (setgladder: any, nickname: string | undefined) =
 			.then((data) => data.json())
 			.then((data) => {
 				setgladder(data);
+
 			});
 		}, []);
 };
@@ -73,9 +74,9 @@ const useGetUserdata = async (setdashstate: any, nickname: string | undefined) =
 export default function Dashboard() {
 	const user = useContext(currentUser);
 	const [dashstate, setdashstate] = useState<IUser | null>(null);
-	const [gladder, setgladder] =  useState<IUser | null>(null);
-	const [fladder, setfladder] =  useState<IUser | null>(null);
-	const [gdata, setgdata] =  useState<IUser | null>(null);
+	const [gladder, setgladder] =  useState<IUser []| null>(null);
+	const [fladder, setfladder] =  useState<IUser []| null>(null);
+	const [gamesdata, setgdata] =  useState<Histo[] | null>(null);
 	const params = useParams();
 
 	const nickname = params.nickname ? params.nickname : user?.nickname;
@@ -90,9 +91,9 @@ export default function Dashboard() {
 		<div className="flex flex-col gap-y-16 mt-16">
 			<ProfileDiv who={who} usr={dashstate} func={setdashstate} />
 			<Carousel />
-			<Stats />
+			<Stats History={gamesdata} />
 			{who ? <Ladder GLadder={gladder} FLadder={fladder} /> : null}
-			<History History={gdata} />
+			<History History={gamesdata} />
 			<Footer />
 		</div>
 	);
