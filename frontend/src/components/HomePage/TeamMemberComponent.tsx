d493f8de5bf9e9596f9ec515552bacf587a9778f
Aymane-1 <aechafii@student.1337.ma>
2024-01-11 05:57:16 +0100

    -fix:homepage tweaks

--- frontend/src/components/HomePage/TeamMemberComponent.tsx
+++ frontend/src/components/HomePage/TeamMemberComponent.tsx
@@ -40,11 +40,11 @@ export const TeamLinks = [
 
 export default function TeamMember(props: any){
 	return (
-		<div className={`Member ${props.Color} transition duration-500 ease-in-out font-pixelify max-w-[1536px] min-[0px]:place-content-center lg:px-4 min-[730px]:flex-row lg:flex min-[0px]:mx-5 border-solid border-4 border-black shadow-[2px_4px_0px_0px_#000301] min-[0px]:p-7`}>
-		  <div className={`${props.order} border-solid border-4 border-black bg-white min-[0px]:p-4 sm:p-5 md:p-6 flex place-content-center min-[0px]:w-[40%] sm:w-[35%] min-[0px]:ml-[13%] lg:m-auto lg:w-[30%] lg:mx-9`}>
-		  	<img src={ProfilePic} alt='pic' className=' m-4 sm:w-[320px] lg:w-[330px]md:p-12 border-solid border-black border-4 bg-DefaultColor'></img >
+		<div className={`Member ${props.Color} transition duration-500 ease-in-out font-pixelify max-w-[1536px] min-[0px]:place-content-center lg:px-4 min-[730px]:flex-row lg:flex  border-solid border-4 border-black shadow-[2px_4px_0px_0px_#000301] p-7`}>
+		  <div className={` ${props.order} border-solid border-4 border-black bg-white min-[0px]:p-4 sm:p-5 md:p-6 flex place-content-center min-[0px]:w-[40%] sm:w-[35%] min-[0px]:ml-4 lg:m-auto lg:w-[30%] lg:mx-9`}>
+		  	<img src={ProfilePic} alt='pic' className=' m-4 sm:w-[320px] lg:w-[330px] md:p-12 border-solid border-black border-4 bg-DefaultColor '></img >
 		  </div>
-		  <div className='flex-col justify-center w-[75%] lg:p-7 m-auto '>
+		  <div className='flex-col mx-4 w-[95%] lg:w-[75%] lg:p-7 lg:m-auto '>
 			<h1 className='min-[0px]:text-xl md:text-2xl lg:text-3xl xl:text-5xl my-3'>{props.name}</h1>
 			<Socials TeamLinks={TeamLinks}/>
 		   <p className='min-[0px]:text-md md:text-xl xl:text-2xl my-3'> {props.description} </p>
