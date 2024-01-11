8839938b7e289acef7fe206f3b3ec869bf94bf49
Aymane-1 <aechafii@student.1337.ma>
2024-01-11 20:34:35 +0100

    -fix: width repartition of ladder divs

--- frontend/src/components/Dashboard/Ladder/LadderComponent.tsx
+++ frontend/src/components/Dashboard/Ladder/LadderComponent.tsx
@@ -2,7 +2,7 @@ import Contestant from "./LadderContestant";
 
 export default function Global({ Ladder, LadderTitle }: { Ladder: any; LadderTitle: any }) {
 	return (
-		<div className="max-w-[1536px] m-8 p-10 border-solid border-4 h-[60rem] Ft shadow-[2px_4px_0px_0px_#000301]">
+		<div className="max-w-[1536px] m-8 p-10 border-solid border-4 h-[60rem] Ft shadow-[2px_4px_0px_0px_#000301] w-[50%]">
 			<h1 className="min-[0px]:text-lg xl:text-2xl text-3xl text-center font-Nova font-black mb-1 border-solid border-4 p-6 bg-white shadow-[2px_4px_0px_0px_#000301]">
 				{LadderTitle}
 			</h1>
