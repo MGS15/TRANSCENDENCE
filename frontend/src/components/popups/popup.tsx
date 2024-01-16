974f4de61b3e888a13c4a6c281410130cface1a7
Aymane-1 <aechafii@student.1337.ma>
2024-01-16 18:59:47 +0100

    -fix: Registration PopUp Responsiveness and Styling Tweaks

--- frontend/src/components/popups/popup.tsx
+++ frontend/src/components/popups/popup.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import karontdo from "../../assets/42.png";
+import { AnimatedElement } from "../game/GameSetup";
 
 interface PopupProps {
 	onClose: () => void;
@@ -46,39 +47,38 @@ const Popup: React.FC<PopupProps> = ({ onClose, setPopupSignUpVisible, setPopupS
 		console.log("her");
 	};
 
-	useEffect(() => {
-		document.body.style.overflow = "hidden";
-		return () => {
-			document.body.style.overflow = "unset";
-		};
-	}, []);
+	// useEffect(() => {
+	// 	document.body.style.overflow = "hidden";
+	// 	return () => {
+	// 		document.body.style.overflow = "unset";
+	// 	};
+	// }, []);
 	return (
 		<div className="fixed z-10 inset-0  flex items-center justify-center bg-black bg-opacity-50">
-			<div className="bg-DefaultColor p-6 rounded-lg shadow-xl m-[12%] max-sm:max-w-[90%]" >
-				<div className="b-50 m-[11%] mx-auto  max-sm:max-w-[90%] flex">
-					<div className="w-[45%]">
-						<img src={karontdo} className="w-[345px]"></img>
+			<div className="bg-DefaultColor p-6  font-Nova border-solid border-8 border-black shadow-[2px_4px_0px_0px_#000301] m-[12%] max-sm:max-w-[90%]" >
+				<div className="b-50 m-[11%] mx-auto  max-sm:max-w-[90%] flex justify-evenly">
+				<div className="hidden w-[40%] lg:flex">
+						<AnimatedElement/>
 					</div>
 
-					<div className="border-solid flex flex-col justify-center text-center gap-y-10">
-						<p className="font-bold text-3xl">Sign in, and let the games begin!</p>
-						<p className="font-bold text-1xl ">
+					<div className="border-solid flex flex-col justify-around  items-center text-center gap-y-10 w-[95%] md:w-[70%] lg:w-[45%]">
+						<p className="font-extrabold text-xl md:text-3xl animate-pulse">Sign in, and let the games begin!</p>
+						<p className="font-bold text-sm md:text-xl lg:text-center">
 							Ping-pong pal! It's been a minute, huh Recall the pixel paddle days? Get ready for a
 							nostalgia wave your fave ping-pong platform beckons!
 						</p>
-						<div className=" md:mt-32 flex justify-center align-center">
+						{/* <div className="flex justify-center"> */}
 							<button
 								onClick={KarontdoIntra}
-								className="flex gap-x-5 bg-yellow-500 text-black py-2 md:px-40 max-sm:px-20 rounded-full shadow-2xl"
-							>
-								<img src={karontdo} className="w-[30px]"></img>
-								NETWORK
+								className="flex md:gap-x-5 border-solid bg-buttonColor text-black border-black shadow-[2px_4px_0px_0px_#000301] border-2 justify-center items-center w-[90%] sm:w-[55%] md:w-[50%] lg:w-[65%] sm:gap-x-2 p-2">
+								<img src={karontdo} className="h-[2rem] w-[2rem] mx-2 sm:mx-4 md:mx-0"></img>
+								<p className="font-bold text-xs sm:text-base">NETWORK</p>
 							</button>
-						</div>
+						{/* </div> */}
 					</div>
 				</div>
 
-				<button onClick={onClose} className="bg-yellow-500 text-black py-2 px-5 rounded-full shadow-2xl">
+				<button onClick={onClose} className="p-2 font-bold text-xs md:text-xl border-2 border-solid bg-buttonColor text-black border-black shadow-[2px_4px_0px_0px_#000301] w-[35%] sm:w-[20%]">
 					{" "}
 					Close{" "}
 				</button>
