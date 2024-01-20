68faaf6f570dbf427b7736530fcacee6c596477f
7ach7ouch_101 <mahmoud_meziani@outlook.sa>
2024-01-20 05:28:07 +0100

    -fix: refactore

--- frontend/src/components/HomePage/SocialMediaComponent.tsx
+++ frontend/src/components/HomePage/SocialMediaComponent.tsx
@@ -10,19 +10,19 @@ export default function SocialMedia({TeamLinks} : {TeamLinks : any}){
 	i++;
 	return(
 		<div className='SocialMedia flex gap-3'>
-
-			{/* <a href={TeamLinks[i].githubLink} target="_blank">
+			{TeamLinks[i]?.githubLink ? <a href={TeamLinks[i]?.githubLink} target="_blank">
 		 		 <img src={GithubLogo} alt='pic'></img>
-			</a>
-			<a href={TeamLinks[i].LinkedinLink} target="_blank">
-				<img src={LinkedinLogo} alt='pic'></img>
-			</a>
-			<a href={TeamLinks[i].InstagramLink} target="_blank">
-				<img src={InstagramLogo} alt='pic'></img>
-			</a>
-			<a href={TeamLinks[i].XLink} target="_blank">
-				<img src={Xlogo} alt='pic'></img>
-			</a> */}
+			</a> : null}
+			{TeamLinks[i]?.InstagramLink ? <a href={TeamLinks[i]?.InstagramLink} target="_blank">
+		 		 <img src={InstagramLogo} alt='pic'></img>
+			</a> : null}
+			{TeamLinks[i]?.LinkedinLink ? <a href={TeamLinks[i]?.LinkedinLink} target="_blank">
+		 		 <img src={LinkedinLogo} alt='pic'></img>
+			</a> : null}
+			{TeamLinks[i]?.XLink ? <a href={TeamLinks[i]?.XLink} target="_blank">
+		 		 <img src={Xlogo} alt='pic'></img>
+			</a> : null}
+			
 		</div>
 	);
 }
--- frontend/src/components/HomePage/TeamMemberComponent.tsx
+++ frontend/src/components/HomePage/TeamMemberComponent.tsx
@@ -7,7 +7,7 @@ export const TeamLinks = [
 		githubLink : "https://github.com/MGS15",
 		InstagramLink : "https://instagram.com/soufiane.mgs",
 		LinkedinLink : "https://linkedin.com/soufiane.mgs",
-		XLink : "https://x.com/soufiane.mgs",
+		XLink : "",
 	},
 	{
 		name : "TAHA EL MOUHAJIR",
@@ -19,28 +19,29 @@ export const TeamLinks = [
 	{
 		name : "OUAIL ZAHIR",
 		githubLink : "https://github.com/waelzahir",
-		InstagramLink : "https://github.com/MGS15",
-		LinkedinLink : "https://github.com/ozahir",
+		InstagramLink : "",
+		LinkedinLink : "",
+		XLink : "", 
 	},
 	{
 		name : "AYMANE ECHAFII",
 		githubLink : "https://github.com/aymane-1",
-		InstagramLink : "https://github.com/MGS15",
-		LinkedinLink : "https://github.com/7ach7ouch101",
-		XLink : "https://x.com/aymane.echafii", 
+		InstagramLink : "https://instagram.com/MGS15",
+		LinkedinLink : "",
+		XLink : "https://x.com/aymane_echafii", 
 	},
 	{
 		name : "MAHMOUD MEZIANI",
 		githubLink : "https://github.com/7ach7ouch101",
 		InstagramLink : "https://github.com/7ach7ouch101",
-		LinkedinLink : "https://github.com/7ach7ouch101",
-		XLink : "https://x.com/7ach7ouch101",
+		LinkedinLink : "",
+		XLink : "",
 	},
 ]
 
 export default function TeamMember(props: any){
 	return (
-		<div className={`Member ${props.Color} transition duration-500 ease-in-out font-pixelify max-w-[1536px] min-[0px]:place-content-center lg:px-4 min-[730px]:flex-row lg:flex min-[0px]:mx-5 border-solid border-4 border-black shadow-[2px_4px_0px_0px_#000301] min-[0px]:p-7`}>
+		<div className={`Member ${props.Color} transition duration-500 ease-in-out font-pixelify max-w-[1536px] min-[0px]:place-content-center lg:px-4 lg:flex min-[0px]:mx-5 border-solid border-4 border-black shadow-[2px_4px_0px_0px_#000301] min-[0px]:p-7`}>
 		  <div className={`${props.order} border-solid border-4 border-black bg-white min-[0px]:p-4 sm:p-5 md:p-6 flex place-content-center min-[0px]:w-[40%] sm:w-[35%] min-[0px]:ml-[13%] lg:m-auto lg:w-[30%] lg:mx-9`}>
 		  	<img src={ProfilePic} alt='pic' className=' m-4 sm:w-[320px] lg:w-[330px]md:p-12 border-solid border-black border-4 bg-DefaultColor'></img >
 		  </div>
