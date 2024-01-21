cd3acd0d2c6e83bb26ff73c149a5ba7fd30e4816
7ach7ouch_101 <mahmoud_meziani@outlook.sa>
2024-01-21 04:01:43 +0100

    tweaks

--- frontend/src/components/notifbar/NotificationBar.tsx
+++ frontend/src/components/notifbar/NotificationBar.tsx
@@ -57,16 +57,15 @@ const NotificationBar = ({ toogle, settogle, status }: { toogle: number; settogl
 
 			<ToggleButton isOpen={isOpen} isNewAlert={newAlert} setIsOpen={toggleChatBar} />
 
-			<section
-				className={`fixed inset-y-0 right-0 bg-background border-l-2 border-solid 
+			<div
+				className={`fixed  inset-y-0 right-0 bg-background border-l-2 border-solid p-4
 			sm:w-[85%] md:w-1/2 lg:w-1/2 xl:w-[35%] 2xl:w-[30%] w-[90%] transition-all duration-300
 			font-pixelify z-50
 			${isOpen ? "transform translate-x-0" : "transform translate-x-full"}`}
 			>
-				<div className={`flex flex-col gap-2 h-full max-h-full min-h-full p-1 divide-x`}>
+				<div className={`flex flex-col gap-2 h-full max-h-full min-h-full p-1`}>
 					<div
-						className={`grid grid-cols-10 gap-1 p-1 h-[92px] min-h-[92px] content-evenly
-					cursor-pointer`}
+						className={`flex gap-x-16 p-1 min-h-[92px] cursor-pointer justify-content`}
 					>
 						<div className={`col-span-2`}>
 							<img
@@ -104,7 +103,7 @@ const NotificationBar = ({ toogle, settogle, status }: { toogle: number; settogl
 						{invites}
 					</div>
 				</div>
-			</section>
+			</div>
 		</>
 	);
 };
