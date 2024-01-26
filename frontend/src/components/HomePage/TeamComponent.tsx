a22f2347700459cdd9979c40f1c33e341e5ea956
7ach7ouch_101 <mahmoud_meziani@outlook.sa>
2024-01-26 20:53:23 +0100

    added pics about team members

--- frontend/src/components/HomePage/TeamComponent.tsx
+++ frontend/src/components/HomePage/TeamComponent.tsx
@@ -1,5 +1,11 @@
 import TeamMembers from "./TeamMemberComponent";
 import Spliter from "../Spliter";
+import Choppa from "../../assets/choppa.png"
+import Frank from "../../assets/frank.png"
+import Sanji from "../../assets/sanji.png"
+import Zoro from "../../assets/zoro.png"
+import luffy from "../../assets/luffy.png"
+
 
 export default function Team() {
 	return (
@@ -7,30 +13,35 @@ export default function Team() {
 			<Spliter title="OUR TEAM" />
 			<div className="flex flex-col gap-y-12 max-w-[1536px] min-[0px]:mx-12 2xl:m-auto">
 				<TeamMembers
+					pic={Choppa}
 					Color={"bg-primary"}
 					order=""
 					name="SOUFIANE ELKHAMLICHI"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed risus at nulla porttitor interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam a velit sodales pellentesque. Nulla congue, eros non vestibulum malesuada, risus nisl interdum elit, et sagittis nulla sapien congue leo. Duis sit amet sagittis odio, vel facilisis urna. Suspendisse sed ipsum nec ex tincidunt iaculis. Curabitur eget sagittis nunc, eget luctus quam."
+					description="MGS, With his consistency and dedication put into his work, Can be highly effective in a team. By consistently meeting deadlines and delivering work on time, He demonstrates a diverse skill set encompassing front-end development, Back-end development, Design, And code structure."
 				/>
 				<TeamMembers
+					pic={Frank}
 					Color={"bg-buttonColor"}
 					order={"order-last"}
 					name="TAHA EL MOUHAJIR"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed risus at nulla porttitor interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam a velit sodales pellentesque. Nulla congue, eros non vestibulum malesuada, risus nisl interdum elit, et sagittis nulla sapien congue leo. Duis sit amet sagittis odio, vel facilisis urna. Suspendisse sed ipsum nec ex tincidunt iaculis. Curabitur eget sagittis nunc, eget luctus quam."
+					description="PAJI1, With his quick thinking and problem-solving skills, Possesses the ability to effectively lead the team in a positive direction. His astute decision-making and proactive approach enable him to navigate challenges efficiently. AND HE LIKES EGGS FOR SURE. "
 				/>
 				<TeamMembers
+					pic={Zoro}
 					Color={"bg-JacobsBlueColor"}
 					order=""
 					name="OUAIL ZAHIR"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed risus at nulla porttitor interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam a velit sodales pellentesque. Nulla congue, eros non vestibulum malesuada, risus nisl interdum elit, et sagittis nulla sapien congue leo. Duis sit amet sagittis odio, vel facilisis urna. Suspendisse sed ipsum nec ex tincidunt iaculis. Curabitur eget sagittis nunc, eget luctus quam."
+					description="WAEIL, "
 				/>
 				<TeamMembers
+					pic={Sanji}
 					Color={"bg-secondary"}
 					order={"order-last"}
 					name="AYMANE ECHAFII"
 					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed risus at nulla porttitor interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam a velit sodales pellentesque. Nulla congue, eros non vestibulum malesuada, risus nisl interdum elit, et sagittis nulla sapien congue leo. Duis sit amet sagittis odio, vel facilisis urna. Suspendisse sed ipsum nec ex tincidunt iaculis. Curabitur eget sagittis nunc, eget luctus quam."
 				/>
 				<TeamMembers
+					pic={luffy}
 					Color={"bg-errorColor"}
 					order=""
 					name="MAHMOUD MEZIANI"
